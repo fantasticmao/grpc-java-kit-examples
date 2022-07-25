10a2929f1e09bc20b9159de745064c1f5daa3a36
fantasticmao <maomao8017@gmail.com>
2022-07-26 02:41:50 +0800

    Feat: examples for using gRPC-Java-Kit

--- bin/helloworld_client.sh
+++ bin/helloworld_client.sh
@@ -10,6 +10,7 @@ java -javaagent:opentelemetry-javaagent.jar \
   -Dotel.traces.exporter=otlp \
   -Dotel.exporter.otlp.traces.endpoint=http://localhost:4317 \
   -Dotel.exporter.otlp.traces.protocol=grpc \
-  -Dotel.metrics.exporter=none \
+  -Dotel.metrics.exporter=prometheus \
+  -Dotel.exporter.prometheus.port=9465 \
   -Dotel.logs.exporter=none \
   -jar helloworld-client-1.0.0-SNAPSHOT.jar "${NAME}"
--- bin/helloworld_server.sh
+++ bin/helloworld_server.sh
@@ -5,6 +5,7 @@ java -javaagent:opentelemetry-javaagent.jar \
   -Dotel.traces.exporter=otlp \
   -Dotel.exporter.otlp.traces.endpoint=http://localhost:4317 \
   -Dotel.exporter.otlp.traces.protocol=grpc \
-  -Dotel.metrics.exporter=none \
+  -Dotel.metrics.exporter=prometheus \
+  -Dotel.exporter.prometheus.port=9464 \
   -Dotel.logs.exporter=none \
   -jar helloworld-server-grpc-1.0.0-SNAPSHOT.jar
