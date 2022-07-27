4b0993adb42362f9714e576bf328cf97bbbf2d4f
fantasticmao <maomao8017@gmail.com>
2022-07-28 03:51:40 +0800

    Feat: examples for using gRPC-Java-Kit

--- bin/helloworld_client.sh
+++ bin/helloworld_client.sh
@@ -5,12 +5,14 @@ if [ -z "${NAME}" ]; then
   NAME='Guest'
 fi
 
-java -javaagent:opentelemetry-javaagent.jar \
-  -Dotel.service.name=helloworld_client \
-  -Dotel.traces.exporter=otlp \
-  -Dotel.exporter.otlp.traces.endpoint=http://localhost:4317 \
-  -Dotel.exporter.otlp.traces.protocol=grpc \
-  -Dotel.metrics.exporter=prometheus \
-  -Dotel.exporter.prometheus.port=9465 \
-  -Dotel.logs.exporter=none \
-  -jar helloworld-client-1.0.0-SNAPSHOT.jar "${NAME}"
+SERVICE_NAME="helloworld_client"
+OPEN_TELEMETRY_AGENT="opentelemetry-javaagent.jar"
+
+# Enable OpenTelemetry
+JVM_OPTS="${JVM_OPTS} -javaagent:${OPEN_TELEMETRY_AGENT} -Dotel.service.name=${SERVICE_NAME} \
+-Dotel.traces.exporter=otlp \
+-Dotel.exporter.otlp.traces.endpoint=http://localhost:4317 -Dotel.exporter.otlp.traces.protocol=grpc \
+-Dotel.metrics.exporter=none \
+-Dotel.logs.exporter=none"
+
+java ${JVM_OPTS} -jar helloworld-client-1.0.0-SNAPSHOT.jar "${NAME}"
--- bin/helloworld_server.sh
+++ bin/helloworld_server.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env sh
 
-java -javaagent:opentelemetry-javaagent.jar \
-  -Dotel.service.name=helloworld_server \
-  -Dotel.traces.exporter=otlp \
-  -Dotel.exporter.otlp.traces.endpoint=http://localhost:4317 \
-  -Dotel.exporter.otlp.traces.protocol=grpc \
-  -Dotel.metrics.exporter=prometheus \
-  -Dotel.exporter.prometheus.port=9464 \
-  -Dotel.logs.exporter=none \
-  -jar helloworld-server-grpc-1.0.0-SNAPSHOT.jar
+SERVICE_NAME="helloworld_server"
+OPEN_TELEMETRY_AGENT="opentelemetry-javaagent.jar"
+
+# Enable JMX
+JVM_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9999 \
+-Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false \
+-Djava.rmi.server.hostname=localhost"
+
+# Enable OpenTelemetry
+JVM_OPTS="${JVM_OPTS} -javaagent:${OPEN_TELEMETRY_AGENT} -Dotel.service.name=${SERVICE_NAME} \
+-Dotel.traces.exporter=otlp \
+-Dotel.exporter.otlp.traces.endpoint=http://localhost:4317 -Dotel.exporter.otlp.traces.protocol=grpc \
+-Dotel.metrics.exporter=prometheus -Dotel.exporter.prometheus.port=9464 \
+-Dotel.logs.exporter=none"
+
+java ${JVM_OPTS} -jar helloworld-server-grpc-1.0.0-SNAPSHOT.jar
